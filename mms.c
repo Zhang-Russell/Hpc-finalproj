a2d1c60a45b30b09b316a458415136a2f412cd2e
Zhang Yifei <12432360@mail.sustech.edu.cn>
2025-06-19 21:08:12 +0800

    change 2d problem to 1d

--- mms.c
+++ mms.c
@@ -1,4 +1,4 @@
-static char help[] = "Final, All-in-One Parallel 2D Heat Solver with Physics/MMS modes, Explicit/Implicit schemes, HDF5 Restart, and VTK output.\n\n";
+static char help[] = "Final, complete 1D Parallel Heat Solver with All Features: Physics/MMS, Explicit/Implicit, HDF5 Restart, and VTK output.\n\n";
 
 #include <petscdm.h>
 #include <petscdmda.h>
@@ -13,23 +13,17 @@ const char *const RunTypes[] = {"physics", "mms", "RunType", "RUN_", 0};
 typedef enum {TS_IMPLICIT, TS_EXPLICIT} TimeSteppingType;
 const char *const TimeSteppingTypes[] = {"implicit", "explicit", "TimeSteppingType", "TS_", 0};
 
-// Helper function to enforce boundary conditions on a vector
+// Helper function to enforce boundary conditions for the 1D rod
 PetscErrorCode EnforceBoundaryConditions(DM da, Vec u)
 {
-    // Our chosen manufactured solution also has homogeneous (zero) boundary conditions,
-    // so this function works for both physics and MMS modes.
     PetscErrorCode ierr;
     DMDALocalInfo  info;
-    PetscScalar    **u_local;
+    PetscScalar    *u_local;
     ierr = DMDAGetLocalInfo(da, &info);CHKERRQ(ierr);
     ierr = DMDAVecGetArray(da, u, &u_local);CHKERRQ(ierr);
-    for (PetscInt j = info.ys; j < info.ys + info.ym; j++) {
-        for (PetscInt i = info.xs; i < info.xs + info.xm; i++) {
-            if (i == 0 || i == info.mx - 1 || j == 0 || j == info.my - 1) {
-                u_local[j][i] = 0.0;
-            }
-        }
-    }
+    // Set BCs at the two ends of the rod
+    if (info.xs == 0) u_local[0] = 0.0; // Left end (x=0)
+    if (info.xs + info.xm == info.mx) u_local[info.xm - 1] = 0.0; // Right end (x=1)
     ierr = DMDAVecRestoreArray(da, u, &u_local);CHKERRQ(ierr);
     return 0;
 }
@@ -43,7 +37,7 @@ int main(int argc, char **args) {
     PetscErrorCode      ierr;
     RunType             run_type = RUN_PHYSICS;
     TimeSteppingType    ts_type = TS_IMPLICIT;
-    PetscInt            nx = 51, ny = 51, max_steps = 200, start_step = 0;
+    PetscInt            nx = 101, max_steps = 200, start_step = 0;
     PetscReal           final_time = 0.2, diffusivity = 0.01, dt = 0.0;
     PetscBool           flg_restart = PETSC_FALSE;
     char                checkpoint_file[] = "checkpoint.h5";
@@ -51,11 +45,10 @@ int main(int argc, char **args) {
 
     /* --- Initialization and Option Parsing --- */
     ierr = PetscInitialize(&argc, &args, (char*)0, help); CHKERRQ(ierr);
-    PetscOptionsBegin(PETSC_COMM_WORLD, "", "Heat Equation Solver Options", "");
+    PetscOptionsBegin(PETSC_COMM_WORLD, "", "1D Heat Equation Solver Options", "");
     ierr = PetscOptionsEnum("-run_type", "Running mode (physics or mms)", "main", RunTypes, (PetscEnum)run_type, (PetscEnum*)&run_type, NULL); CHKERRQ(ierr);
     ierr = PetscOptionsEnum("-ts_type", "Time stepping method", "main", TimeSteppingTypes, (PetscEnum)ts_type, (PetscEnum*)&ts_type, NULL);CHKERRQ(ierr);
     ierr = PetscOptionsInt("-nx", "Grid points in x direction", "main", nx, &nx, NULL);CHKERRQ(ierr);
-    ierr = PetscOptionsInt("-ny", "Grid points in y direction", "main", ny, &ny, NULL);CHKERRQ(ierr);
     ierr = PetscOptionsReal("-final_time", "Final simulation time", "main", final_time, &final_time, NULL);CHKERRQ(ierr);
     ierr = PetscOptionsInt("-max_steps", "Number of time steps (used if -dt is not set)", "main", max_steps, &max_steps, NULL);CHKERRQ(ierr);
     ierr = PetscOptionsReal("-dt", "Time step size (overrides max_steps)", "main", dt, &dt, NULL);CHKERRQ(ierr);
@@ -63,16 +56,15 @@ int main(int argc, char **args) {
     PetscOptionsEnd();
 
     /* --- DMDA and Vector Setup --- */
-    ierr = DMDACreate2d(PETSC_COMM_WORLD, DM_BOUNDARY_NONE, DM_BOUNDARY_NONE, DMDA_STENCIL_STAR, nx, ny, PETSC_DECIDE, PETSC_DECIDE, 1, 1, NULL, NULL, &da); CHKERRQ(ierr);
+    ierr = DMDACreate1d(PETSC_COMM_WORLD, DM_BOUNDARY_NONE, nx, 1, 1, NULL, &da); CHKERRQ(ierr);
     ierr = DMSetUp(da); CHKERRQ(ierr);
-    ierr = DMDASetUniformCoordinates(da, 0.0, 1.0, 0.0, 1.0, 0.0, 0.0); CHKERRQ(ierr);
+    ierr = DMDASetUniformCoordinates(da, 0.0, 1.0, 0.0, 0.0, 0.0, 0.0); CHKERRQ(ierr);
     ierr = DMCreateGlobalVector(da, &u); CHKERRQ(ierr);
-    ierr = PetscObjectSetName((PetscObject)u, "Numerical_Solution"); CHKERRQ(ierr);
+    ierr = PetscObjectSetName((PetscObject)u, "Temperature"); CHKERRQ(ierr);
     ierr = VecDuplicate(u, &u_old); CHKERRQ(ierr);
     if (run_type == RUN_MMS) {
         ierr = VecDuplicate(u, &f_source); CHKERRQ(ierr);
         ierr = VecDuplicate(u, &u_exact); CHKERRQ(ierr);
-        ierr = PetscObjectSetName((PetscObject)u_exact, "Exact_Solution"); CHKERRQ(ierr);
     }
 
     /* --- Initial State --- */
@@ -96,21 +88,19 @@ int main(int argc, char **args) {
         ierr = MPI_Bcast(&start_step, 1, MPIU_INT, 0, PETSC_COMM_WORLD); CHKERRQ(ierr);
         ierr = VecDestroy(&v_step); CHKERRQ(ierr);
         ierr = PetscViewerDestroy(&hdf5_viewer); CHKERRQ(ierr);
-        ierr = PetscPrintf(PETSC_COMM_WORLD, "Successfully restarted from step %d\n", start_step); CHKERRQ(ierr);
+        ierr = PetscPrintf(PETSC_COMM_WORLD, "Successfully restarted from step %ld\n", (long)start_step); CHKERRQ(ierr);
     } else {
         DMDALocalInfo info;
-        PetscScalar   **u_local;
-        PetscReal     dx = 1.0/(nx-1), dy = 1.0/(ny-1);
+        PetscScalar   *u_local;
+        PetscReal     dx = 1.0/(nx-1);
         ierr = DMDAGetLocalInfo(da, &info); CHKERRQ(ierr);
         ierr = DMDAVecGetArray(da, u, &u_local); CHKERRQ(ierr);
-        for (PetscInt j=info.ys; j<info.ys+info.ym; j++) {
-            for (PetscInt i=info.xs; i<info.xs+info.xm; i++) {
-                PetscReal x=i*dx, y=j*dy;
-                if (run_type == RUN_PHYSICS) {
-                    if (x>=0.4&&x<=0.6&&y>=0.4&&y<=0.6) u_local[j][i] = 100.0; else u_local[j][i] = 0.0;
-                } else { // RUN_MMS
-                    u_local[j][i] = sin(PETSC_PI * x) * sin(PETSC_PI * y);
-                }
+        for (PetscInt i=info.xs; i<info.xs+info.xm; i++) {
+            PetscReal x = i*dx;
+            if (run_type == RUN_PHYSICS) {
+                if (x>=0.4 && x<=0.6) u_local[i] = 100.0; else u_local[i] = 0.0;
+            } else { // RUN_MMS
+                u_local[i] = sin(PETSC_PI * x);
             }
         }
         ierr = DMDAVecRestoreArray(da, u, &u_local); CHKERRQ(ierr);
@@ -119,22 +109,19 @@ int main(int argc, char **args) {
 
     /* --- Pre-Loop Setup --- */
     if (dt > 0.0) { max_steps = (PetscInt)(final_time / dt); } else { dt = final_time / max_steps; }
+    PetscReal dx = 1.0 / (nx - 1);
     
     if (ts_type == TS_IMPLICIT) {
-        ierr = PetscPrintf(PETSC_COMM_WORLD, "Setting up for IMPLICIT method, dt = %g, steps = %d\n", (double)dt, max_steps);CHKERRQ(ierr);
+        ierr = PetscPrintf(PETSC_COMM_WORLD, "Setting up for 1D IMPLICIT method...\n");CHKERRQ(ierr);
         ierr = DMCreateMatrix(da, &A); CHKERRQ(ierr);
-        DMDALocalInfo info; ierr = DMDAGetLocalInfo(da, &info); CHKERRQ(ierr);
-        MatStencil row, col[5]; PetscScalar v[5];
-        PetscReal dx = 1.0/(nx-1), dy = 1.0/(ny-1), alpha_x = diffusivity*dt/(dx*dx), alpha_y = diffusivity*dt/(dy*dy);
-        for (PetscInt j = info.ys; j < info.ys + info.ym; j++) {
-            for (PetscInt i = info.xs; i < info.xs + info.xm; i++) {
-                row.j = j; row.i = i;
-                if (i==0||i==nx-1||j==0||j==ny-1) { v[0] = 1.0; ierr = MatSetValuesStencil(A, 1, &row, 1, &row, v, INSERT_VALUES); CHKERRQ(ierr); }
-                else {
-                    v[0]=1.0+2.0*alpha_x+2.0*alpha_y; col[0]=row; v[1]=-alpha_x; col[1].j=j; col[1].i=i-1; v[2]=-alpha_x; col[2].j=j; col[2].i=i+1;
-                    v[3]=-alpha_y; col[3].j=j-1; col[3].i=i; v[4]=-alpha_y; col[4].j=j+1; col[4].i=i;
-                    ierr = MatSetValuesStencil(A, 1, &row, 5, col, v, INSERT_VALUES); CHKERRQ(ierr);
-                }
+        PetscReal alpha = diffusivity * dt / (dx * dx);
+        for (PetscInt i = 0; i < nx; i++) {
+            if (i == 0 || i == nx - 1) {
+                ierr = MatSetValue(A, i, i, 1.0, INSERT_VALUES); CHKERRQ(ierr);
+            } else {
+                PetscScalar vals[3] = {-alpha, 1.0 + 2.0 * alpha, -alpha};
+                PetscInt    cols[3] = {i - 1, i, i + 1};
+                ierr = MatSetValues(A, 1, &i, 3, cols, vals, INSERT_VALUES); CHKERRQ(ierr);
             }
         }
         ierr = MatAssemblyBegin(A, MAT_FINAL_ASSEMBLY); CHKERRQ(ierr); ierr = MatAssemblyEnd(A, MAT_FINAL_ASSEMBLY); CHKERRQ(ierr);
@@ -142,24 +129,17 @@ int main(int argc, char **args) {
         ierr = KSPSetOperators(ksp, A, A); CHKERRQ(ierr);
         ierr = KSPSetFromOptions(ksp); CHKERRQ(ierr);
     } else { // Explicit
-        ierr = PetscPrintf(PETSC_COMM_WORLD, "Setting up for EXPLICIT method, dt = %g, steps = %d\n", (double)dt, max_steps);CHKERRQ(ierr);
-        PetscReal dx = 1.0/(nx-1), dy = 1.0/(ny-1);
-        PetscReal stability_factor = diffusivity * dt * (1.0/(dx*dx) + 1.0/(dy*dy));
-        ierr = PetscPrintf(PETSC_COMM_WORLD, "Stability Factor: %g (must be <= 0.5)\n", (double)stability_factor); CHKERRQ(ierr);
+        ierr = PetscPrintf(PETSC_COMM_WORLD, "Setting up for 1D EXPLICIT method...\n");CHKERRQ(ierr);
+        PetscReal stability_factor = diffusivity * dt / (dx * dx);
+        ierr = PetscPrintf(PETSC_COMM_WORLD, "Stability Factor: %g (must be <= 0.5 for 1D)\n", (double)stability_factor); CHKERRQ(ierr);
         if (stability_factor > 0.5) { ierr = PetscPrintf(PETSC_COMM_WORLD, "WARNING: Stability condition not met!\n");CHKERRQ(ierr); }
         ierr = DMCreateMatrix(da, &L); CHKERRQ(ierr);
         ierr = VecDuplicate(u, &laplacian_u); CHKERRQ(ierr);
-        DMDALocalInfo info; ierr = DMDAGetLocalInfo(da, &info); CHKERRQ(ierr);
-        MatStencil row, col[5]; PetscScalar v[5];
-        for (PetscInt j = info.ys; j < info.ys + info.ym; j++) {
-            for (PetscInt i = info.xs; i < info.xs + info.xm; i++) {
-                row.j = j; row.i = i;
-                if (i==0||i==nx-1||j==0||j==ny-1) { v[0] = 0.0; ierr = MatSetValuesStencil(L, 1, &row, 1, &row, v, INSERT_VALUES); CHKERRQ(ierr); }
-                else {
-                    v[0]=-2.0/(dx*dx)-2.0/(dy*dy); col[0]=row; v[1]=1.0/(dx*dx); col[1].j=j; col[1].i=i-1; v[2]=1.0/(dx*dx); col[2].j=j; col[2].i=i+1;
-                    v[3]=1.0/(dy*dy); col[3].j=j-1; col[3].i=i; v[4]=1.0/(dy*dy); col[4].j=j+1; col[4].i=i;
-                    ierr = MatSetValuesStencil(L, 1, &row, 5, col, v, INSERT_VALUES); CHKERRQ(ierr);
-                }
+        for (PetscInt i = 0; i < nx; i++) {
+            if (i > 0 && i < nx - 1) {
+                PetscScalar vals[3] = {1.0/(dx*dx), -2.0/(dx*dx), 1.0/(dx*dx)};
+                PetscInt    cols[3] = {i - 1, i, i + 1};
+                ierr = MatSetValues(L, 1, &i, 3, cols, vals, INSERT_VALUES); CHKERRQ(ierr);
             }
         }
         ierr = MatAssemblyBegin(L, MAT_FINAL_ASSEMBLY); CHKERRQ(ierr); ierr = MatAssemblyEnd(L, MAT_FINAL_ASSEMBLY); CHKERRQ(ierr);
@@ -167,33 +147,35 @@ int main(int argc, char **args) {
     
     /* --- Main Time-Stepping Loop --- */
     for (PetscInt step = start_step; step < max_steps; ++step) {
-        PetscReal current_time = (step + 1) * dt;
+        PetscReal time_n = step * dt;
+        PetscReal time_np1 = (step + 1) * dt;
 
         if (run_type == RUN_MMS) {
-            DMDALocalInfo info; ierr = DMDAGetLocalInfo(da, &info); CHKERRQ(ierr);
-            PetscScalar **f_local; ierr = DMDAVecGetArray(da, f_source, &f_local); CHKERRQ(ierr);
-            PetscReal dx = 1.0/(nx-1), dy = 1.0/(ny-1);
-            for (PetscInt j = info.ys; j < info.ys + info.ym; j++) {
-                for (PetscInt i = info.xs; i < info.xs + info.xm; i++) {
-                    PetscReal x = i * dx, y = j * dy;
-                    f_local[j][i] = (2.0*diffusivity*PETSC_PI*PETSC_PI - 1.0) * sin(PETSC_PI*x) * sin(PETSC_PI*y) * exp(-current_time);
-                }
+            PetscScalar *f_local;
+            DMDALocalInfo info;
+            ierr = DMDAGetLocalInfo(da, &info); CHKERRQ(ierr);
+            ierr = DMDAVecGetArray(da, f_source, &f_local); CHKERRQ(ierr);
+            PetscReal f_time = (ts_type == TS_IMPLICIT) ? time_np1 : time_n;
+            for (PetscInt i = info.xs; i < info.xs + info.xm; i++) {
+                PetscReal x = i * dx;
+                f_local[i] = (diffusivity*PETSC_PI*PETSC_PI - 1.0) * sin(PETSC_PI*x) * exp(-f_time);
             }
             ierr = DMDAVecRestoreArray(da, f_source, &f_local); CHKERRQ(ierr);
         }
 
         if (ts_type == TS_IMPLICIT) {
             ierr = VecCopy(u, u_old); CHKERRQ(ierr);
-            if (run_type == RUN_MMS) { ierr = VecAXPY(u_old, dt, f_source); CHKERRQ(ierr); } // Add source to RHS
+            if (run_type == RUN_MMS) { ierr = VecAXPY(u_old, dt, f_source); CHKERRQ(ierr); }
             ierr = KSPSolve(ksp, u_old, u); CHKERRQ(ierr);
         } else { // Explicit
             ierr = MatMult(L, u, laplacian_u); CHKERRQ(ierr);
-            ierr = VecAXPY(u, dt * diffusivity, laplacian_u); CHKERRQ(ierr); // Diffusion part
-            if (run_type == RUN_MMS) { ierr = VecAXPY(u, dt, f_source); CHKERRQ(ierr); } // Source part
+            ierr = VecAXPY(u, dt * diffusivity, laplacian_u); CHKERRQ(ierr);
+            if (run_type == RUN_MMS) { ierr = VecAXPY(u, dt, f_source); CHKERRQ(ierr); }
             ierr = EnforceBoundaryConditions(da, u);CHKERRQ(ierr);
         }
 
-        if ((step + 1) % checkpoint_interval == 0 && (step + 1) < max_steps) {
+        /* --- Full-featured Output Logic (Inside the loop) --- */
+        if (checkpoint_interval > 0 && (step + 1) % checkpoint_interval == 0 && (step + 1) < max_steps) {
             PetscViewer hdf5_viewer;
             ierr = PetscPrintf(PETSC_COMM_WORLD, "Writing checkpoint at step %ld to %s\n", (long)(step + 1), checkpoint_file); CHKERRQ(ierr);
             ierr = PetscViewerHDF5Open(PETSC_COMM_WORLD, checkpoint_file, FILE_MODE_WRITE, &hdf5_viewer); CHKERRQ(ierr);
@@ -225,30 +207,26 @@ int main(int argc, char **args) {
     /* --- Post-Loop: Error Calculation for MMS --- */
     if (run_type == RUN_MMS) {
         DMDALocalInfo info;
-        PetscScalar   **uexact_local;
-        PetscReal     dx = 1.0/(nx-1), dy = 1.0/(ny-1);
+        PetscScalar *uexact_local;
         ierr = DMDAGetLocalInfo(da, &info); CHKERRQ(ierr);
         ierr = DMDAVecGetArray(da, u_exact, &uexact_local); CHKERRQ(ierr);
-        for (PetscInt j = info.ys; j < info.ys + info.ym; j++) {
-            for (PetscInt i = info.xs; i < info.xs + info.xm; i++) {
-                PetscReal x = i * dx, y = j * dy;
-                uexact_local[j][i] = sin(PETSC_PI * x) * sin(PETSC_PI * y) * exp(-final_time);
-            }
+        for (PetscInt i = info.xs; i < info.xs + info.xm; i++) {
+            PetscReal x = i * dx;
+            uexact_local[i] = sin(PETSC_PI * x) * exp(-final_time);
         }
         ierr = DMDAVecRestoreArray(da, u_exact, &uexact_local); CHKERRQ(ierr);
         
         PetscReal error_norm;
-        ierr = VecAXPY(u, -1.0, u_exact); // u = u - u_exact (error vector)
+        ierr = VecAXPY(u, -1.0, u_exact); // u = u - u_exact
         ierr = VecNorm(u, NORM_INFINITY, &error_norm); CHKERRQ(ierr);
         ierr = PetscPrintf(PETSC_COMM_WORLD, "--------------------------------------------------\n"); CHKERRQ(ierr);
-        ierr = PetscPrintf(PETSC_COMM_WORLD, "MMS Verification Result:\n"); CHKERRQ(ierr);
+        ierr = PetscPrintf(PETSC_COMM_WORLD, "MMS Verification Result (1D):\n"); CHKERRQ(ierr);
         ierr = PetscPrintf(PETSC_COMM_WORLD, "L-infinity Error at T=%.2f with nx=%d, dt=%.6f is: %g\n", (double)final_time, nx, (double)dt, (double)error_norm); CHKERRQ(ierr);
         ierr = PetscPrintf(PETSC_COMM_WORLD, "--------------------------------------------------\n"); CHKERRQ(ierr);
     }
     
     /* --- Final Output and Cleanup --- */
-    char filename[PETSC_MAX_PATH_LEN];
-    PetscViewer vtk_viewer;
+    PetscViewer vtk_viewer; char filename[PETSC_MAX_PATH_LEN];
     if (ts_type == TS_IMPLICIT) { ierr = PetscSNPrintf(filename, sizeof(filename), "implicit_solution.vts"); CHKERRQ(ierr); }
     else { ierr = PetscSNPrintf(filename, sizeof(filename), "explicit_solution.vts"); CHKERRQ(ierr); }
     ierr = PetscPrintf(PETSC_COMM_WORLD, "Writing final solution to %s\n", filename); CHKERRQ(ierr);
